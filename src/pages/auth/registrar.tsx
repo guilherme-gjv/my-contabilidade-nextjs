2fd98678f21dff014a1d6ef80a7bb57e9e26861c
guilherme-gjv <guijesus.info@gmail.com>
2023-10-16 14:48:30 -0300

    feat: registrar page

--- src/pages/auth/registrar.tsx
+++ src/pages/auth/registrar.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { getErrorMessage } from "@/functions/getErrorMessage";
 import Link from "next/link";
+import { api } from "@/services/api";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { "mycontabilidade.token": token } = parseCookies(ctx);
@@ -27,21 +28,42 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   };
 };
 
-const validationSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: "O e-mail é obrigatório" })
-    .email("E-mail inválido"),
-  password: z
-    .string({
-      required_error: "O campo 'password' é obrigatório.",
-      description: "O campo 'email' deve ser string.",
-    })
-    .min(8, { message: "A senha deve possuir ao menos 8 caracteres." }),
-});
-type ILoginFormData = z.infer<typeof validationSchema>;
+const validationSchema = z
+  .object({
+    email: z
+      .string({
+        required_error: "O campo 'email' é obrigatório.",
+      })
+      .email({ message: "email inválido." }),
+    cpf: z
+      .string({ description: "O campo 'CPF' é obrigatório." })
+      .refine(
+        (data) => data.length === 11 || data.length === 14 || data.length === 0,
+        {
+          message: "O CPF ou CNPJ deve possuir 11 ou 14 dígitos.",
+        }
+      )
+      .optional(),
+    name: z
+      .string({
+        required_error: "O campo 'name' é obrigatório",
+      })
+      .min(4, { message: "O nome deve ter pelo menos 4 caracteres." }),
+    password: z
+      .string({
+        required_error: "O campo 'password' é obrigatório.",
+      })
+      .min(8, { message: "A senha deve possuir ao menos 8 caracteres." }),
+    confirm_password: z.string(),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "As senhas não combinam.",
+    path: ["confirm_password"],
+  });
 
-const Login: React.FC = () => {
+type ISignupFormData = z.infer<typeof validationSchema>;
+
+const Signup: React.FC = () => {
   //* hooks
   const { push } = useRouter();
   const { signIn } = useContext(AuthContext);
@@ -49,24 +71,27 @@ const Login: React.FC = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ILoginFormData>({ resolver: zodResolver(validationSchema) });
+  } = useForm<ISignupFormData>({ resolver: zodResolver(validationSchema) });
 
   //* callbacks
   const handleOnSubmit = useCallback(
-    async ({ email, password }: ILoginFormData) => {
+    async ({ name, email, cpf, password }: ISignupFormData) => {
       const { updateToast } = customToast(
-        "Autenticando",
+        "Registrando",
         CustomToastTypes.LOADING
       );
       try {
-        await signIn({ email, password });
+        console.log("carregando");
+
+        await api.post("/user", { name, email, cpf, password });
         updateToast({
-          render: "Autenticado com sucesso!",
+          render: "Registrado com sucesso!",
           type: "success",
           isLoading: false,
           autoClose: 5000,
         });
-        push("/");
+
+        push("/auth/login");
       } catch (error) {
         updateToast({
           render: "Houve um erro no login! " + getErrorMessage(error),
@@ -76,7 +101,7 @@ const Login: React.FC = () => {
         });
       }
     },
-    [signIn, push]
+    [push]
   );
 
   //* render
@@ -91,12 +116,33 @@ const Login: React.FC = () => {
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Entre em sua conta
+          Registrar
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" onSubmit={handleSubmit(handleOnSubmit)}>
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Nome
+            </label>
+            <div className="mt-2">
+              <input
+                id="name"
+                {...register("name")}
+                type="name"
+                autoComplete="name"
+                required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors.name && (
+                <p className="mt-1 text-red-500">{errors.name?.message}</p>
+              )}
+            </div>
+          </div>
           <div>
             <label
               htmlFor="email"
@@ -115,6 +161,26 @@ const Login: React.FC = () => {
               />
               {errors.email && (
                 <p className="mt-1 text-red-500">{errors.email?.message}</p>
+              )}
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="cpf"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              CPF/CPNJ {"(opcional)"}
+            </label>
+            <div className="mt-2">
+              <input
+                id="cpf"
+                {...register("cpf")}
+                type="cpf"
+                autoComplete="cpf"
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors.cpf && (
+                <p className="mt-1 text-red-500">{errors.cpf?.message}</p>
               )}
             </div>
           </div>
@@ -141,12 +207,45 @@ const Login: React.FC = () => {
                 id="password"
                 {...register("password")}
                 type="password"
-                autoComplete="current-password"
+                autoComplete="password"
                 required
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
               {errors.password && (
                 <p className="mt-1 text-red-500">{errors.password?.message}</p>
+              )}
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-start">
+              <label
+                htmlFor="confirm_password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Confirmar Senha
+              </label>
+              {/* {<div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>} */}
+            </div>
+            <div className="mt-2">
+              <input
+                id="confirm_password"
+                {...register("confirm_password")}
+                type="password"
+                autoComplete="confirm_password"
+                required
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors.confirm_password && (
+                <p className="mt-1 text-red-500">
+                  {errors.confirm_password?.message}
+                </p>
               )}
             </div>
           </div>
@@ -156,19 +255,19 @@ const Login: React.FC = () => {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Entrar
+              Registrar
             </button>
           </div>
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
-          Não tem uma conta?
+          Já tem uma conta?
           <Link
-            href={"/auth/registrar"}
+            href={"/auth/login"}
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >
             {" "}
-            registre-se gratuitamente
+            login
           </Link>
         </p>
       </div>
@@ -176,4 +275,4 @@ const Login: React.FC = () => {
   );
 };
 
-export default Login;
+export default Signup;
