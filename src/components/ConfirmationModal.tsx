989f5709624b0e098993b5159e98fe0311bcfb9a
guilherme-gjv <guijesus.info@gmail.com>
2023-10-22 18:15:22 -0300

    fix: specifying type on confirmation modal buttons

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -46,12 +46,14 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
           <div className="flex items-center gap-x-3">
             <button
               onClick={onClose}
+              type="button"
               className="leading-6 text-gray-900 rounded-md bg-white px-3 py-2 text-sm font-semibold shadow-sm transition-colors hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               {denyLabel}
             </button>
             <button
               onClick={onConfirm}
+              type="button"
               className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
             >
               {confirmLabel}
