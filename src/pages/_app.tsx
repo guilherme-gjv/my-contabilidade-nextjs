31b7783ac1ea77dd1eaf3b568bc897ffd3ffd269
guilherme-gjv <guijesus.info@gmail.com>
2023-10-26 11:37:25 -0300

    feat: tab title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import Providers from "@/partials/Providers";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
+      <Head>
+        <title>My Contabilidade</title>
+      </Head>
       <Component {...pageProps} />
     </Providers>
   );
